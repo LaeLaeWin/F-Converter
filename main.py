efd99727ae6ab01bfd2a32e9e28dd35af65b8e69
LaeLaeWin <emolay491997@gmail.com>
2018-08-20 19:55:27 -0700

    changed file

--- main.py
+++ main.py
@@ -2,39 +2,39 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
-@app.route('/cvstyle.html')
+@app.route('/')
 def cvstyle():
     return render_template('cvstyle.html')
 
-@app.route('/dropdown.html')
+@app.route('/dropdown')
 def dropdown():
     return render_template('dropdown.html')
 
-@app.route('/dropdown2.html')
+@app.route('/dropdown2')
 def dropdown2():
     return render_template('dropdown2.html')
 
-@app.route('/dropdown3.html')
+@app.route('/dropdown3')
 def dropdown3():
     return render_template('dropdown3.html')
 
-@app.route('/dropdown4.html')
+@app.route('/dropdown4')
 def dropdown4():
     return render_template('dropdown4.html')
 
-@app.route('/dropdown5.html')
+@app.route('/dropdown5')
 def dropdown5():
     return render_template('dropdown5.html')
 
-@app.route('/dropdown6.html')
+@app.route('/dropdown6')
 def dropdown6():
     return render_template('dropdown6.html')
 
-@app.route('/signup1.html')
+@app.route('/signup1')
 def signup1():
     return render_template('signup1.html')
 
-@app.route('/signin1.html')
+@app.route('/signin1')
 def signin1():
     return render_template('signin1.html')
 
